459372cc4bd32e869539751967a109c97b574bce
Pratik Nayak <pratik18karate12345@gmail.com>
2024-10-14 14:55:44 +0530

    created a separate nav graph file

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.example.retrofit
 
-import DateOfBirthScreen
-import FitnessStatusScreen
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,22 +9,9 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.retrofit.screen.setup.LandingScreen
-import com.example.retrofit.screen.Screen
-import com.example.retrofit.screen.setup.ResetPasswordScreen
-import com.example.retrofit.screen.setup.LoginScreen
-import com.example.retrofit.screen.setup.SignUpScreen
-import com.example.retrofit.screen.setup.SplashScreen
-import com.example.retrofit.screen.userdetails.GenderScreen
-import com.example.retrofit.screen.userdetails.HomeScreen
-import com.example.retrofit.screen.userdetails.UserMetricsScreen
 import com.example.retrofit.ui.theme.RetroFitTheme
 import com.example.retrofit.viewmodel.AuthViewModel
 
